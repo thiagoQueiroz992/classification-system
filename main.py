f9df96277c6f14d50ab28f481e6008873a7ddff6
Thiago Queiroz <thsqueiroz.2k26@gmail.com>
2026-01-31 09:58:36 -0300

    name_asking

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import courses
 
+name = "null"
+subscription_number = 0
 scores = {"linguagens": 0, "matemática": 0, "ciências da natureza": 0, "ciências humanas": 0, "redação": 0}
+score_mean = 0
+
+while True:
+    try:
+        name = str(input("Nome:\n")).title()
+    except:
+        print("Valor inválido!")
+        continue
+    else:
+        break
+
 for s in scores.keys():
     scores[s] = float(input(f"Qual foi sua nota em {s.title()}? "))
